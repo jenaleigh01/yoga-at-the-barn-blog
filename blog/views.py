d2162bdc41f360de9a532268adc6cc5bfa4d594e
Jenna Guest <jenaleigh01@gmail.com>
2024-05-25 14:26:49 +0000

    Installed crispy forms, added comment form and comment notification

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -29,18 +31,18 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
-        print("About to render a template")
-        if comment_form_is_valid():
+        if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.post = post
             comment.save()
             messages.add_message(
                 request, messages.SUCCESS,
-                'Your comment has been submitted and awaiting approval'
+                'Your comment has been submitted and awaiting approvel'
             )
+          
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -48,9 +50,9 @@ def post_detail(request, slug):
         {
             "post": post,
             "comments": comments,
-            "comment_count": comment_count
+            "comment_count": comment_count,
+            "comment_form": comment_form,
         },
-
 
     )
 
